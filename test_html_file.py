2c0d92e785ecea6e2e3fcbaa01174fc25030d0ea
AmazingSauce <AmazingSauce@gmail.com>
2016-08-12 16:26:46 -0600

    testing creating html file

--- test_html_file.py
+++ test_html_file.py
@@ -1,2 +1,22 @@
 #!/usr/bin/env python
+#test file to test making html file checklist with pythong.
+
+# link using to create html file with python
+# http://programminghistorian.org/lessons/creating-and-viewing-html-files-with-python
+
+
+f = open('helloworld.html', 'w')
+
+case_number = raw_input("Enter your case number: ")
+
+message = """<html>
+<head></head>
+<Mbody><p>"""+ case_number +"""</p></body>
+</html>
+"""
+
+f.write(message)
+f.close()
+
+
 
